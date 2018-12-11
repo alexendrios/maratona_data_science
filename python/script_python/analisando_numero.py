93c497e45534fa316b2abdd6db3dec6535896133
Alexandre Pereira Santos <alex.endrios@hotmail.com>
2018-12-11 07:53:22 -0200

    analisando numero

--- python/script_python/analisando_numero.py
+++ python/script_python/analisando_numero.py
@@ -1,10 +1,15 @@
 #entrada
-numero = int(input('Informe o numero a ser analisado de 0 a 9999'))
-n = str(numero)
+numero = int(input('Informe o numero a ser analisado de 0 a 9999: '))
 
 #processamento
-milhar = n[0]
-centena = n[1]
-dezena = n[2]
-unidade = n[3]
-print(milhar)
+u = numero // 1 % 10
+d = numero // 10 % 10
+c = numero // 100 % 10
+m = numero // 1000 % 10
+
+#milhar
+print('\nAnalisando o Número {}'.format(numero))
+print('Unidade:\t{}'.format(u))
+print('Dezena:\t\t{}'.format(d))
+print('Centena:\t{}'.format(c))
+print('Milhara:\t{}'.format(m))
